cc296aefaa1256c80571c8f69f0a6daa7136666e
JoseZapata3 <josezapata75235@gmail.com>
2024-09-17 00:25:01 -0500

    feat: The Models in Phoenix section is completed. A model, a schema and a form are created.

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :discuss, Discuss.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Zen@75235_jz",
   hostname: "localhost",
   database: "discuss_dev",
   stacktrace: true,
